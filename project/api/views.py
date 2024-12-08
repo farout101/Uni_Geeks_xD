14be4df888b6d8e88d254ae9f8fb47a89382af06
Phyo Zaw Linn <phyozawlinn1852020@gmail.com>
2024-12-08 19:39:43 +0630

    add getRooms function and fix import in API views

--- project/api/views.py
+++ project/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from prject
 
 @api_view(['GET'])
 def getRoutes(reqeust):
@@ -9,3 +10,7 @@ def getRoutes(reqeust):
         'GET /api/rooms/:id',
     ]
     return Response(routes)
+
+
+def getRooms(request):
+    return Response(rooms)

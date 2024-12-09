dbac97be1c75c2b6d2df9ae8e13991a8e64c46b7
Hacker Mg Pyuue <phyozawlinn1852020@gmail.com>
2024-12-09 10:34:02 +0630

    final commit

--- project/api/urls.py
+++ project/api/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.getRoutes),
     path('rooms/', views.getRooms),
+    path('room/<str:pk>/', views.getRoom),
 ]
+ 
--- project/api/views.py
+++ project/api/views.py
@@ -8,7 +8,7 @@ def getRoutes(reqeust):
     routes= [
         'GET /api',
         'GET /api/rooms',
-        'GET /api/rooms/:id',
+        'GET /api/room/:id',
     ]
     return Response(routes)
 
@@ -16,4 +16,10 @@ def getRoutes(reqeust):
 def getRooms(request):
     rooms = Room.objects.all()
     serializer = RoomSerializer(rooms, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    serializer = RoomSerializer(room, many=False)
     return Response(serializer.data)

275b6722d5091102e2255d393af6bc341afc3cc1
Hacker Mg Pyuue <phyozawlinn1852020@gmail.com>
2024-12-09 14:54:09 +0630

    remove unused import from views.py

--- project/views.py
+++ project/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
-from fastapi import Response
 from . models import Room, Topic, Message
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
